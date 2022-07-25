2b032bee524d0e9edd85d908bf859c7f776e00a6
guillaume <guillaume.augustoni@epfl.ch>
2022-07-25 22:02:42 +0200

    Ajout coeur

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,19 @@
-import Head from "next/head"
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import Head from "next/head";
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
         <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/red-heart.png" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Je t'aime!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+            Romromromrom ❤️
         </p>
       </main>
 
