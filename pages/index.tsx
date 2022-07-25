f5ac21d19a7f9354b48ffcfe9ff7e53716206ee3
guillaume <guillaume.augustoni@epfl.ch>
2022-07-25 22:08:18 +0200

    Ajout coeur

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Coucou!</title>
         <link rel="icon" href="/red-heart.png" />
       </Head>
 
